280a02125fae1ddf91dc8513d7285575369c90e0
David Huarina <davidhuarina25@gmail.com>
2021-08-02 14:12:35 -0400

    actualizaicon datos envio 1

--- layouts/home.php
+++ layouts/home.php
@@ -19,8 +19,8 @@
         <div class="col-lg-3 col-md-8 mb-5 mb-lg-0 mx-auto">
          <a href="modulo.php?codigo=2" class="after-loop-item card border-0 card-themes shadow-lg">
             <div class="card-body d-flex align-items-end flex-column text-right">
-               <h4>SISTEMA ALMACEN</h4>
-               <p class="w-85">Gestión de personal & Gestión de planillas & Contabilización.</p>
+               <h4>SISTEMA ALMACENES</h4>
+               <p class="w-85">Gestión de productos e ingresos</p>
                <i class="material-icons">local_atm</i>
             </div>
          </a>
@@ -29,7 +29,7 @@
          <a href="modulo.php?codigo=3" class="after-loop-item card border-0 card-templates shadow-lg">
             <div class="card-body d-flex align-items-end flex-column text-right">
                <h4>SISTEMA FINANCIERO</h4>
-               <p class="w-85">Modulo Base de Contabilidad & Caja Chica.</p>
+               <p class="w-85">Modulo Base de Contabilidad y RRHH</p>
                <i class="material-icons">insert_chart_outlined</i>
             </div>
          </a>
@@ -37,8 +37,8 @@
         <div class="col-lg-3 col-md-8 mb-5 mb-lg-0 mx-auto">
          <a href="modulo.php?codigo=4" class="after-loop-item card border-0 card-guides shadow-lg">
             <div class="card-body d-flex align-items-end flex-column text-right">
-               <h4>MONITOREO</h4>
-               <p class="w-75"> Sol. de Recursos & Contabilización.</p>
+               <h4>TIENDA VIRTUAL / PAGINA WEB</h4>
+               <p class="w-75"> Modulo de configuracion.</p>
                <i class="material-icons">list_alt</i>
             </div>
          </a>
